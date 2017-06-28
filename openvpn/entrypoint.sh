cb949893072330af9274f99182e547929fa5a412
Cellophan <githubcom.cell@outer.systems>
2017-06-28 15:38:53 +0200

    ADD prohibit default for openvpn

--- openvpn/entrypoint.sh
+++ openvpn/entrypoint.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-[ -d /dev/net ] ||
-	mkdir -p /dev/net
-[ -c /dev/net/tun ] ||
-	mknod /dev/net/tun c 10 200
+[ -d /dev/net ] || mkdir -p /dev/net
+[ -c /dev/net/tun ] || mknod /dev/net/tun c 10 200
 
-echo 1 > /proc/sys/net/ipv4/ip_forward
+REMOTE=$(grep ^remote *ovpn | cut -d\  -f2)
+GATEWAY=$(ip route get 8.8.8.8 | grep via | sed 's/.*via \([.0-9]*\) .*/\1/')
+
+ip route add ${REMOTE} via ${GATEWAY}
+ip route del default
+ip route add prohibit default
+#echo 1 > /proc/sys/net/ipv4/ip_forward
 
 exec openvpn --config *.ovpn
 
